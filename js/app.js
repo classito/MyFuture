ea5f641c23cc1a35830e3a7dcb84b655b70547e1
Kamui <30941329+KingKamui@users.noreply.github.com>
2018-02-21 14:16:00 -1000

    I believe that's how the button works

---
+++ js/app.js
@@ -0,0 +1,5 @@
+function Raisec_Test(Link) {
+    document.getElementById("Raisec_Test_Link").innerHTML = "Put Raisec Test Link Here Jeraldy";
+}
+
+//Jeraldy, the Raisec Test Link Goes here I belive, I'm not effecient at JS a lot to know that... ;(
