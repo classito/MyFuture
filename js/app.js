d741f66eea39ac37853db3d4cd4238fc22c46230
Kamui <30941329+KingKamui@users.noreply.github.com>
2018-02-21 14:29:48 -1000

    Button is not neccessary

--- js/app.js
+++ js/app.js
@@ -4,4 +4,4 @@ function Raisec_Test(Link) {
 
 //Jeraldy, the Raisec Test Link Goes here I belive, I'm not effecient at JS a lot to know that... ;(
 
-//Scratch all of JS for this page, the button is not neccessary, it will be removed later
+//Scratch all of JS for this page, the button is not neccessary, it will be removed later, instead I'll use a scr link
