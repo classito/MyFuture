ff14c37c2b94ecd90ab2c0f81e7c28b40f1f3c12
Kamui <30941329+KingKamui@users.noreply.github.com>
2018-02-21 14:35:37 -1000

    Misspell Riasec

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
-function Raisec_Test(Link) {
-    document.getElementById("Raisec_Test_Link").innerHTML = "Put Raisec Test Link Here Jeraldy";
+function Riasec_Test(Link) {
+    document.getElementById("Riasec_Test_Link").innerHTML = "Put Riasec Test Link Here Jeraldy";
 }
 
-//Jeraldy, the Raisec Test Link Goes here I belive, I'm not effecient at JS a lot to know that... ;(
+//Jeraldy, the Riasec Test Link Goes here I belive, I'm not effecient at JS a lot to know that... ;(
 
 //Scratch all of JS for this page, the button is not neccessary, it will be removed later, instead I'll use a scr link
