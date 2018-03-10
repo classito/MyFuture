87f7a273d4f8eb7f7be658bef4db8c9849a6914d
c0p <casourcer@gmail.com>
2018-03-09 14:02:43 -1000

    temporarily

---
+++ js/classesDB.js
@@ -0,0 +1,20 @@
+var classesDB = {
+	I : {
+		classes: [    ["calculus", "precalculus", "any", "GR12" ],   "computer science", "physics", "calculus"]
+	},
+	E : {
+		classes: ["business", "finance", "memes"]
+	},
+
+	thisFunction: function(user) {
+		if (user[1] === "I") {
+			return classesDB.I.classes[0]
+		}
+	}
+}
+
+var jameson = ["R", "I", "C"];
+
+
+console.log(classesDB.thisFunction(jameson));;
+
