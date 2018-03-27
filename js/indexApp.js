6e7963ed97d6ace32f83284d90b0a3facbc3a797
c0p <jeraldydcascayan@gmail.com>
2018-03-26 17:21:26 -1000

    merge

--- js/indexApp.js
+++ js/indexApp.js
@@ -131,5 +131,39 @@ function showPage() {
     //disables loader and content appears
     document.getElementById("loader").style.display = "none";
     document.getElementById("myDiv").style.display = "block";
+}
 
+//Slideshow Code
+
+
+//Another Slide Show Code
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
 }
