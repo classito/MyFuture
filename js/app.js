ffa0b0dfd4f7005de97f342555a973fc8bbb96cb
Kamui <30941329+KingKamui@users.noreply.github.com>
2018-04-21 16:56:47 -1000

    Delete app.js

--- js/app.js
+++
@@ -1,7 +0,0 @@
-function Riasec_Test(Link) {
-    document.getElementById("Riasec_Test_Link").innerHTML = "Put Riasec Test Link Here Jeraldy";
-}
-
-//Jeraldy, the Riasec Test Link Goes here I belive, I'm not effecient at JS a lot to know that... ;(
-
-//Scratch all of JS for this page, the button is not neccessary, it will be removed later, instead I'll use a scr link
